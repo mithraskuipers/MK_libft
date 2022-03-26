7ab7b0410927906bfb9612c57a37d5ab21e0ffea
Mithras Kuipers <mithraskuipers@gmail.com>
2022-03-26 22:39:27 +0100

    update

--- mk_putnbr_base.c
+++ mk_putnbr_base.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_putnbr_base.c                                   :+:    :+:            */
+/*   mk_putnbr_base.c                                   :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/03/22 21:35:32 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/03/22 21:35:32 by mikuiper      ########   odam.nl         */
+/*   Created: 2022/03/26 22:39:09 by mikuiper      #+#    #+#                 */
+/*   Updated: 2022/03/26 22:39:09 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_base(int nbr, int base)
+void	mk_putnbr_base(int nbr, int base)
 {
 	int		result;
 	char	print;
@@ -31,6 +31,6 @@ void	ft_putnbr_base(int nbr, int base)
 		print = set[nbr % base];
 	}
 	if (result != 0)
-		ft_putnbr_base(result, base);
+		mk_putnbr_base(result, base);
 	write(1, &print, 1);
 }
